1991771ebcac6a32d38c1bb42fbf6a1596f26eae
Tom Perrau <tomperrau@macbook-pro-tom-1.home>
2020-07-17 18:38:58 +0200

    coucou

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector("#contact-form");
 
   console.log(form);
-  document
-    .querySelector("#contact-form")
-    .addEventListener("submit", async (event) => {
-      console.log("coucou1");
-      event.preventDefault();
-      console.log("coucou2");
-      const data = {
-        firstname: document.querySelector("#firstname").value,
-        lastname: document.querySelector("#lastname").value,
-        email: document.querySelector("#email").value,
-        subject: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value,
-      };
-      console.log("coucou3");
-      console.log(data);
-      const response = await axios.post(
-        "https://contact-form-backend-tom.herokuapp.com/form",
-        data
-      );
-      console.log(response);
-    });
+  // document
+  //   .querySelector("#contact-form")
+  form.addEventListener("submit", async (event) => {
+    console.log("coucou1");
+    event.preventDefault();
+    console.log("coucou2");
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
+    console.log("coucou3");
+    console.log(data);
+    const response = await axios.post(
+      "https://contact-form-backend-tom.herokuapp.com/form",
+      data
+    );
+    console.log(response);
+  });
 });
