08a8ae5ec937e16335b69c02fab03f1bd07c5347
Tom Perrau <tomperrau@macbook-pro-tom-1.home>
2020-07-17 18:35:24 +0200

    coucou

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("#contact-form")
     .addEventListener("submit", async (event) => {
+      console.log("coucou1");
       event.preventDefault();
+      console.log("coucou2");
       const data = {
         firstname: document.querySelector("#firstname").value,
         lastname: document.querySelector("#lastname").value,
@@ -12,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
         subject: document.querySelector("#subject").value,
         message: document.querySelector("#message").value,
       };
+      console.log("coucou3");
       console.log(data);
       const response = await axios.post(
         "https://contact-form-backend-tom.herokuapp.com/form",
